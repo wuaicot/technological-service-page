d75f537216f4b7713cf50441e402f7dfbcdfe608
Naycol Linares <wuaicot8@gmail.com>
2024-12-31 15:40:56 -0300

    is coming

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,3 @@
-// File: /app/page.jsx
 import HeroSection from './components/HomePage/HeroSection';
 import SliderTaglines from './components/HomePage/SliderTaglines';
 import BrandSection from './components/HomePage/BrandSection';
@@ -7,9 +6,15 @@ import PlatformSection from './components/HomePage/PlatformSection';
 import EducationSection from './components/HomePage/EducationSection';
 import ContactSection from './components/HomePage/ContactSection';
 
+// Importa las secciones de About Page
+import WhoAreWe from './components/About/WhoAreWe';
+import VisionMission from './components/About/VisionMission';
+import Principles from './components/About/Principles';
+
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-black text-white">
+      {/* Secciones de Home Page */}
       <HeroSection />
       <SliderTaglines />
       <BrandSection />
@@ -17,6 +22,11 @@ export default function HomePage() {
       <PlatformSection />
       <EducationSection />
       <ContactSection />
+
+      {/* Secciones de About Page */}
+      <WhoAreWe />
+      <VisionMission />
+      <Principles />
     </main>
   );
 }
